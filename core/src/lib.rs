d086106a9cf4060cd7228abdc6480830eb62015e
Diabolo <alexis.boitel@epitech.eu>
2025-08-31 14:38:03 +0200

    fix(ci-cd) remove issue sync from mirror to epitech-repo

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -15,4 +15,15 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+
+    #[test]
+    fn test_critique() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[cfg(feature = "experimental")]
+    #[test]
+    fn test_experimental() {
+        assert_eq!(2 + 2, 5); // volontairement faux
+    }
 }
