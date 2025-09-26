98227b4d4026ffe4d2fc6ebb8635b16064ef307b
Diabolo <alexis.boitel@epitech.eu>
2025-09-26 13:44:59 +0900

    feat(gui): base window template

---
+++ gui/src/components/search.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+    Command,
+    CommandDialog,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+    CommandShortcut,
+} from "@/components/ui/command";
+import { Button } from "./ui/button";
+import { SearchCode } from "lucide-react";
+
+function SearchInput() {
+    const [open, setOpen] = React.useState(false);
+
+    React.useEffect(() => {
+        const down = (e: KeyboardEvent) => {
+            if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+                e.preventDefault();
+                setOpen((open) => !open);
+            }
+        };
+        document.addEventListener("keydown", down);
+        return () => document.removeEventListener("keydown", down);
+    }, []);
+
+    return (
+        <div>
+            <Button
+                variant="outline"
+                className="mx-4"
+                onClick={() => setOpen((open) => !open)}
+            >
+                <SearchCode />
+                search
+            </Button>
+            <CommandDialog open={open} onOpenChange={setOpen}>
+                <CommandInput placeholder="Type a command or search..." />
+                <CommandList>
+                    <CommandEmpty>No results found.</CommandEmpty>
+                    <CommandGroup heading="Suggestions">
+                        <CommandItem>Calendar</CommandItem>
+                        <CommandItem>Search Emoji</CommandItem>
+                        <CommandItem>Calculator</CommandItem>
+                    </CommandGroup>
+                </CommandList>
+            </CommandDialog>
+        </div>
+    );
+}
+
+export { SearchInput };
--- gui/src/components/title-bar.tsx
+++ gui/src/components/title-bar.tsx
@@ -3,64 +3,78 @@ import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Copy, Minus, Square, X } from "lucide-react";
 import { getCurrentWindow } from "@tauri-apps/api/window";
+import { SearchInput } from "./search";
 
 function TitleBar() {
-    const appWindow = getCurrentWindow();
+  const appWindow = getCurrentWindow();
 
-    const [isMaximized, setIsMaximized] = useState(false);
+  const [isMaximized, setIsMaximized] = useState(false);
 
-    useEffect(() => {
-        const minimizeButton = document.getElementById("titlebar-minimize");
-        const maximizeButton = document.getElementById("titlebar-maximize");
-        const closeButton = document.getElementById("titlebar-close");
+  useEffect(() => {
+    const minimizeButton = document.getElementById("titlebar-minimize");
+    const maximizeButton = document.getElementById("titlebar-maximize");
+    const closeButton = document.getElementById("titlebar-close");
 
-        // Define event listener functions
-        const handleMinimize = () => appWindow.minimize();
-        const handleMaximize = () => {
-            appWindow.toggleMaximize();
-            setIsMaximized((prev) => !prev);
-        };
-        const handleClose = () => appWindow.close();
+    // Define event listener functions
+    const handleMinimize = () => appWindow.minimize();
+    const handleMaximize = () => {
+      appWindow.toggleMaximize();
+      setIsMaximized((prev) => !prev);
+    };
+    const handleClose = () => appWindow.close();
 
-        // Add event listeners
-        minimizeButton?.addEventListener("click", handleMinimize);
-        maximizeButton?.addEventListener("click", handleMaximize);
-        closeButton?.addEventListener("click", handleClose);
+    // Add event listeners
+    minimizeButton?.addEventListener("click", handleMinimize);
+    maximizeButton?.addEventListener("click", handleMaximize);
+    closeButton?.addEventListener("click", handleClose);
 
-        // Cleanup event listeners on component unmount
-        return () => {
-            minimizeButton?.removeEventListener("click", handleMinimize);
-            maximizeButton?.removeEventListener("click", handleMaximize);
-            closeButton?.removeEventListener("click", handleClose);
-        };
-    }, []);
+    // Cleanup event listeners on component unmount
+    return () => {
+      minimizeButton?.removeEventListener("click", handleMinimize);
+      maximizeButton?.removeEventListener("click", handleMaximize);
+      closeButton?.removeEventListener("click", handleClose);
+    };
+  }, []);
 
-    return (
-        <div
-            className="border px-4 py-0.5 flex items-center"
-            data-tauri-drag-region
-        >
-            <img
-                src="/assets/logo.png"
-                alt="Griffon Logo"
-                style={{
-                    imageRendering: "pixelated",
-                }}
-                className="w-14 h-auto"
-            />
-            <div className="font-bold">Griffon</div>
-            <div className="flex-1"></div>
-            <Button variant={"ghost"} id="titlebar-minimize" title="minimize">
-                <Minus />
-            </Button>
-            <Button variant={"ghost"} id="titlebar-maximize" title="maximize">
-                {isMaximized ? <Copy /> : <Square />}
-            </Button>
-            <Button variant={"ghost"} id="titlebar-close" title="close">
-                <X />
-            </Button>
-        </div>
-    );
+  return (
+    <div className="px-4 py-3 flex items-center" data-tauri-drag-region>
+      <img
+        src="/assets/logo.png"
+        alt="Griffon Logo"
+        style={{
+          imageRendering: "pixelated",
+        }}
+        className="w-14 h-auto"
+      />
+      <div className="font-bold">Griffon</div>
+      <div className="flex-1"></div>
+      <SearchInput></SearchInput>
+      <Button
+        className="cursor-pointer"
+        variant={"ghost"}
+        id="titlebar-minimize"
+        title="minimize"
+      >
+        <Minus />
+      </Button>
+      <Button
+        className="cursor-pointer"
+        variant={"ghost"}
+        id="titlebar-maximize"
+        title="maximize"
+      >
+        {isMaximized ? <Copy /> : <Square />}
+      </Button>
+      <Button
+        className="cursor-pointer"
+        variant={"ghost"}
+        id="titlebar-close"
+        title="close"
+      >
+        <X />
+      </Button>
+    </div>
+  );
 }
 
 export { TitleBar };
