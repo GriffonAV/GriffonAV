c86614bf4b309bee8850bb65edd7640f24e33cbe
Diabolo <alexis.boitel@proton.me>
2025-11-19 17:04:55 +0900

    feat(justfile root) setup gui cmd

--- staticAnalysis/src/main.rs
+++
@@ -1,7 +0,0 @@
-use rustav::{load_yara_rules, scan_file};
-
-fn main() {
-    let rules = load_yara_rules("rules");
-    let hits = scan_file(&rules, "samples/sample_001.txt");
-    println!("Matched {} rules", hits);
-}
