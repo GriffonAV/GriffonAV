a47f44c523e394da5e43e36ef29b29be347ba21e
Diabolo <alexis.boitel@proton.me>
2025-11-27 13:45:31 +0900

    feat(gui): polishing the ui

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { TitleBar } from "./components/title-bar";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
@@ -7,8 +8,8 @@ function App() {
       <div className="flex h-screen flex-col">
         <TitleBar />
         <div className="flex-1 flex flex-row">
-          <div className="flex flex-col w-48">
-            <div className="mt-6"></div>
+          <div className="flex flex-col w-48 p-4">
+            {/* <div className="mt-6"></div>
 
             <div className="mt-2 flex bg-sidebar-accent rounded-none h-10 items-center">
               <div className="bg-red-500 w-2 h-full mr-6"></div>
@@ -21,9 +22,18 @@ function App() {
             <div className="mt-2 flex h-10 items-center">
               <div className="w-2 h-full mr-6"></div>
               <span>extensions</span>
-            </div>
+            </div> */}
+            <span className="mt-6 mb-2 px-4 text-xs text-muted-foreground uppercase">
+              Plugins
+            </span>
+            <Button
+              className="cursor-pointer mb-2"
+              variant={"ghost"}
+            >
+              Analyser
+            </Button>
           </div>
-          <div className="flex h-full w-full bg-secondary text-secondary-foreground p-4 radius-0">
+          <div className="flex h-full w-full border p-4 m-4 radius-0">
             content
           </div>
         </div>
