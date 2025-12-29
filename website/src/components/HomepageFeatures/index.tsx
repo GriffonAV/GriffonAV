979ae62d5d1e73827d27b0df87285d806bad82e5
Diabolo <alexis.boitel@proton.me>
2025-12-29 17:16:19 +0900

    feat(landpage) new cards

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -4,74 +4,77 @@ import Heading from "@theme/Heading";
 import styles from "./styles.module.css";
 
 type FeatureItem = {
-    title: string;
-    // Svg: React.ComponentType<React.ComponentProps<"svg">>;
-    Img: string;
-    description: ReactNode;
+  title: string;
+  // Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  Img: string;
+  description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
-    {
-        title: "Easy to Use",
-        // Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
-        Img: require("@site/static/img/rust.png").default,
-        description: (
-            <>
-                Docusaurus was designed from the ground up to be easily
-                installed and used to get your website up and running quickly.
-            </>
-        ),
-    },
-    {
-        title: "Focus on What Matters",
-        Img: require("@site/static/img/rust.png").default,
-        description: (
-            <>
-                Docusaurus lets you focus on your docs, and we&apos;ll do the
-                chores. Go ahead and move your docs into the <code>docs</code>{" "}
-                directory.
-            </>
-        ),
-    },
-    {
-        title: "Powered by React",
-        Img: require("@site/static/img/rust.png").default,
-        description: (
-            <>
-                Extend or customize your website layout by reusing React.
-                Docusaurus can be extended while reusing the same header and
-                footer.
-            </>
-        ),
-    },
+  {
+    title: "Easy to Use",
+    // Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+    Img: require("@site/static/img/easy-to-use.png").default,
+    description: (
+      <>
+        Griffon was designed from the ground up to be easily installed and
+        to configure.
+      </>
+    ),
+  },
+  {
+    title: "Powered by Rust",
+    Img: require("@site/static/img/rust.png").default,
+    description: (
+      <>
+        Griffon AV is built using Rust, a modern systems programming language
+        known for its performance, safety, and concurrency. This ensures that our
+        autonomous vehicle solutions are both reliable and efficient.
+      </>
+    ),
+  },
+  {
+    title: "Research Oriented and Transparent Project",
+    Img: require("@site/static/img/notion.png").default,
+    description: (
+      <>
+        All our organization's research is open source and transparent. We are
+        committed to making our research accessible to everyone. You can find
+        our notion page{" "}
+        <a href="https://blue-touch-18c.notion.site/Griffon-AV-1c6f05587c8380eb9fbeea36f549fd47?pvs=74">
+          here
+        </a>
+      </>
+    ),
+  },
 ];
 
 function Feature({ title, Img, description }: FeatureItem) {
-    return (
-        <div className={clsx("col col--4")}>
-            <div className="text--center">
-                <img src={Img} className={styles.featureImg} alt={title} />{" "}
-                {/* Use img tag */}
-                {/* <Svg className={styles.featureSvg} role="img" /> */}
-            </div>
-            <div className="text--center padding-horiz--md">
-                <Heading as="h3">{title}</Heading>
-                <p>{description}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={clsx("col col--4")}>
+      <div className="text--center">
+        <img src={Img} className={styles.featureImg} alt={title} />{" "}
+        {/* Use img tag */}
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default function HomepageFeatures(): ReactNode {
-    return (
-        <section className={styles.features}>
-            <div className="container">
-                <div className="row">
-                    {FeatureList.map((props, idx) => (
-                        <Feature key={idx} {...props} />
-                    ))}
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -6,57 +6,92 @@ import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
 import Head from "@docusaurus/Head";
+import Translate, { translate } from "@docusaurus/Translate";
+import useBaseUrl, { useBaseUrlUtils } from "@docusaurus/useBaseUrl";
 
 import styles from "./index.module.css";
 
+function HeroBanner() {
+  const { siteConfig } = useDocusaurusContext();
+
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className={styles.heroProjectTagline}>
+          <img
+            className={styles.heroLogo}
+            src={useBaseUrl("/img/logo.png")}
+            width="200"
+            height="200"
+          />
+          <span
+            className={styles.heroTitleTextHtml}
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: translate({
+                id: "homepage.hero.title",
+                message:
+                  "Build <b>optimized</b> websites <b>quickly</b>, focus on your <b>content</b>",
+                description:
+                  "Home page hero title, can contain simple html tags",
+              }),
+            }}
+          />
+        </Heading>
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            Get Griffon for Linux (AppImage)
+          </Link>
+        </div>
+        <span className={styles.indexCtasGitHubButtonWrapper}>
+          <iframe
+            className={styles.indexCtasGitHubButton}
+            src="https://ghbtns.com/github-btn.html?user=GriffonAV&repo=GriffonAV&type=star&count=true&size=large"
+            width={160}
+            height={30}
+            title="GitHub Stars"
+          />
+        </span>
+      </div>
+    </header>
+  );
+}
+
 function HomepageHeader() {
-    const { siteConfig } = useDocusaurusContext();
-    return (
-        <header className={clsx("hero hero--primary", styles.heroBanner)}>
-            <div className="container">
-                <Heading as="h1" className="hero__title">
-                    {siteConfig.title}
-                </Heading>
-                <p className="hero__subtitle">{siteConfig.tagline}</p>
-                <div className={styles.buttons}>
-                    <Link
-                        className="button button--secondary button--lg"
-                        to="/docs/intro"
-                    >
-                        Get Griffon for Linux (AppImage)
-                    </Link>
-                </div>
-            </div>
-        </header>
-    );
+  const { siteConfig } = useDocusaurusContext();
+  return <HeroBanner />;
 }
 
 export default function Home(): ReactNode {
-    const { siteConfig } = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
 
-    const title = `Griffon | Linux Antivirus`;
-    const description = "Keep it safe. Simple and efficient.";
-    const imageUrl = "https://griffon-av.vercel.app/img/griffon.png";
+  const title = `Griffon | Linux Antivirus`;
+  const description = "Keep it safe. Simple and efficient.";
+  const imageUrl = "https://griffon-av.vercel.app/img/griffon.png";
 
-    return (
-        <Layout title={title} description={description}>
-            <Head>
-                <meta property="og:title" content={title} />
-                <meta property="og:description" content={description} />
-                <meta property="og:image" content={imageUrl} />
-                <meta
-                    property="og:url"
-                    content="https://griffon-av.vercel.app"
-                />
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:title" content={title} />
-                <meta name="twitter:description" content={description} />
-                <meta name="twitter:image" content={imageUrl} />
-            </Head>
-            <HomepageHeader />
-            <main>
-                <HomepageFeatures />
-            </main>
-        </Layout>
-    );
+  return (
+    <Layout title={title} description={description}>
+      <Head>
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={imageUrl} />
+        <meta property="og:url" content="https://griffon-av.vercel.app" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={imageUrl} />
+      </Head>
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
 }
